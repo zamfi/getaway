090f7b28f0bbae3673a26b24cbf6cf4fdf7961b0
Marcell Vazquez-Chanlatte <mvc@linux.com>
2020-06-12 23:00:56 -0700

    Update app.

--- bin/herokuapp.py
+++ bin/herokuapp.py
@@ -10,6 +10,6 @@ import psiturk.experiment_server as exp
 config = configparser.ConfigParser()
 config.read('config.txt')
 sp = config['Server Parameters']
-print(f'Server listening on ' + sp['host'] + ':' + sp['port'])
+print('Server listening on ' + sp['host'] + ':' + sp['port'])
 
 exp.launch()
